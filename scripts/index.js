21a6ea692e57dc8f77ef2c59abcc0f2d259528b1
AlDm <d44.a1@yandex.ru>
2023-03-24 17:43:14 +0300

    сделать меню js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,18 @@
 const clickOpen = document.querySelector('.header__burger');
 const seidbarOpen = document.querySelector('.header__seidbar');
 const clickClose = document.querySelector('.header__seidbar-button');
+const pageOpen = document.querySelector('.header__seidbar-page');
 
+function openPageContent(){
+  pageOpen.classList.add('header__seidbar-page_open');
+}
 
 clickOpen.addEventListener('click', () =>{
     seidbarOpen.classList.add('header__seidbar_open');
+    setTimeout(openPageContent,500);
   })
 
 clickClose.addEventListener('click', () =>{
     seidbarOpen.classList.remove('header__seidbar_open');
+    pageOpen.classList.remove('header__seidbar-page_open');
 })
